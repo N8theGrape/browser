34acf6baaa3698a78c612fa20f3c7fe5f7b5d8b0
Nathan Gillespie <crookedtwig4@gmail.com>
2024-01-14 12:39:19 -0800

    t

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// Simulated data retrieval from a server
-/*async function fetchData() {
-    // Simulating an asynchronous data fetch
-    return [
-        { filename: 'file1.txt', content: '@tags: { "tag1": true, "tag2": false }, @name: "John" ' },
-        { filename: 'file2.txt', content: '@tags: { "tag1": false, "tag2": true }, @name: "Alice" ' },
-        // Add more files as needed
-    ];
-}*/
-
 async function handleKeyPress(event) {
     if (event.keyCode === 13) {
         performSearch();
@@ -25,9 +15,8 @@ async function performSearch() {
 
         // Filter files based on the search term
         var matchingFiles = fileData.filter(function (file) {
-            // Use regular expressions to check if the tag exists and is true
-            var tagRegex = new RegExp('"'+searchTerm+'":\\s*true');
-            return tagRegex.test(file.content);
+            // Check if the file's content includes the search term as a tag
+            return file.content.includes(`@tags: ["${searchTerm}"`);
         });
 
         if (matchingFiles.length > 0) {
--- pageData.js
+++ pageData.js
@@ -1,9 +1,10 @@
+
 // Simulated data retrieval from a server
 async function fetchData() {
     // Simulating an asynchronous data fetch
     return [
-        { filename: 'file1.txt', content: '@tags: { "tag1": true, "tag2": false }, @name: "John" ' },
-        { filename: 'file2.txt', content: '@tags: { "tag1": false, "tag2": true }, @name: "Alice" ' },
+        { filename: 'file1.txt', content: '@tags: ["tag1", "tag2", "tag3"], @name: "John" ' },
+        { filename: 'file2.txt', content: '@tags: ["tag2", "tag4", "tag5"], @name: "Alice" ' },
         // Add more files as needed
     ];
 }
