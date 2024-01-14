13ca897634086c50b7f8973a1f6c06f4e96292ef
Nathan Gillespie <crookedtwig4@gmail.com>
2024-01-14 12:36:00 -0800

    pageData.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Simulated data retrieval from a server
-async function fetchData() {
+/*async function fetchData() {
     // Simulating an asynchronous data fetch
     return [
         { filename: 'file1.txt', content: '@tags: { "tag1": true, "tag2": false }, @name: "John" ' },
         { filename: 'file2.txt', content: '@tags: { "tag1": false, "tag2": true }, @name: "Alice" ' },
         // Add more files as needed
     ];
-}
+}*/
 
 async function handleKeyPress(event) {
     if (event.keyCode === 13) {
